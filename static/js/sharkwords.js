97dacaa9437a65ffffa6a380b40a6edfccb3485c
Natasha Anderson <andersonnatashaa@gmail.com>
2020-10-21 20:17:25 +0000

    functions createDivsForChars, generateLetterButtons, disableLetterButton created

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -19,13 +19,18 @@ let numWrong = 0;
 // Loop over the chars in `word` and create divs.
 //
 const createDivsForChars = (word) => {
-  // Replace this with your code
+  //for loop over word to generate div
+  for (const idx in word) {
+    $('#word-container').append(`<div class=${word[idx]}></div>`);
+    }
 };
 
 // Loop over each letter in `ALPHABET` and generate buttons.
 //
 const generateLetterButtons = () => {
-  // Replace this with your code
+  for (const letter of ALPHABET) {
+    $('#letter-buttons').append(`<button>${letter}</button>`);
+  }
 };
 
 // Set the `disabled` property of `buttonEl` to `true.
@@ -33,7 +38,8 @@ const generateLetterButtons = () => {
 // `buttonEl` is an `HTMLElement` object.
 //
 const disableLetterButton = (buttonEl) => {
-  // Replace this with your code
+  const button = $(buttonEl);
+  $(buttonEl).attr('disabled', 'true');
 };
 
 // Return `true` if `letter` is in the word.
